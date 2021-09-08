0d8b2b0a7bb6cd00e5124f1043a13f5821812881
ItsMe <itsme@bubbleclub.de>
2021-09-08 13:43:01 +0200

    Added some Comments and removed redundant code

--- linux/backup_zfs.sh
+++ linux/backup_zfs.sh
@@ -9,9 +9,9 @@
 # TODO          : a) Implement simple logging capabilities
 #                 b) Implement a proper Errorhandling
 #                 c) Implement commandline options
-#                 d) Implement a proper capacity check
 #
 # Created       : 10|02|2019
+# Version       : 0.1
 #
 # Author        : ItsMe
 # Reply to      : itsme@bubbleclub.de
@@ -52,35 +52,45 @@ TAIL=$(command -v tail)
 
 TARGET="extBackup"                                              # define the Name of the
                                                                 # target Pool
-declare -a PRODUCTION=()                                        # Arrays for local pools
-declare -a ZFS_POOLS=()
-declare -i ZFS_AVAIL                                            # Variable for Space calculation
+declare -a PRODUCTION=()                                        # Array for local pools
+declare -i ZFS_AVAIL                                            # Variables for Space calculation
 declare -i ZFS_USED
 declare -i ZFS_SIZE
 declare -i ZFS_COMBINED
 declare -i ZFS_AVAIL_DEST
 
 function do_space_check(){
-        for i in "${!ZFS_POOLS[@]}"; do
-                echo -e "\n$txt_ok Calculating: $bldblu${ZFS_POOLS[$i]}$txtrst"
-                ZFS_AVAIL=$($ZFS list -oavail,used -t filesystem -pH "${ZFS_POOLS[$i]}" | $AWK '{ print $1 }')
-                ZFS_USED=$($ZFS list -oavail,used -t filesystem -pH "${ZFS_POOLS[$i]}" | $AWK '{ print $2 }')
-                ZFS_SIZE=$((ZFS_AVAIL+ZFS_USED))
+        for i in "${!PRODUCTION[@]}"; do
+                echo -e "\n$txt_ok Calculating: $bldblu${PRODUCTION[$i]}$txtrst"
+                                                                # free space
+                ZFS_AVAIL=$($ZFS list -o avail,used -t filesystem -pH "${PRODUCTION[$i]}" | $AWK '{ print $1 }')
+                                                                # disk space taken
+                ZFS_USED=$($ZFS list -o avail,used -t filesystem -pH "${PRODUCTION[$i]}" | $AWK '{ print $2 }')
+        
+                ZFS_SIZE=$((ZFS_AVAIL+ZFS_USED))                # total size of the pool
+                
                 echo -e "$txt_info Total: $bldgrn$ZFS_SIZE ($($NUMFMT --to=iec-i $ZFS_SIZE)) $txtrst"
                 echo -e "$txt_info Used: $bldgrn$ZFS_USED ($($NUMFMT --to=iec-i $ZFS_USED)) $txtrst"
                 echo -e "$txt_info Free: $bldgrn$ZFS_AVAIL ($($NUMFMT --to=iec-i $ZFS_AVAIL)) $txtrst"
-                ZFS_COMBINED+=ZFS_USED
-        done
-        ZFS_AVAIL_DEST=$($ZFS list -oavail,used -t filesystem -pH "$TARGET" | $AWK '{ print $1 }')
+                
+                ZFS_COMBINED+=ZFS_USED                          # sum of disk space of
+        done                                                    # all local pools
+
+                                                                # get the free and used space of pool(s)
+                                                                # the 'used' property include
+                                                                # the space consumed by snapshots
+        ZFS_AVAIL_DEST=$($ZFS list -o avail,used -t filesystem -pH "$TARGET" | $AWK '{ print $1 }')
         echo -e "$txt_info Used Diskspace on local Pool(s): $bldgrn$ZFS_COMBINED ($($NUMFMT --to=iec-i $ZFS_COMBINED)) $txtrst"
         echo -e "$txt_info Free Diskspace on Target Device: $bldgrn$ZFS_AVAIL_DEST ($($NUMFMT --to=iec-i $ZFS_AVAIL_DEST)) $txtrst"
 
+                                                                # abort when the target hasn't
+                                                                # enough space
         if [ $ZFS_COMBINED -ge $ZFS_AVAIL_DEST ]; then
                 echo -e "\n$txt_wrong$bldred Not enough space on Target Device $txtrst\n$txt_wrong$bldred Aborting..."
                 $ZPOOL export $TARGET                           # export the Pool        
                 exit 3
         else
-                do_sync
+                do_sync                                         # start the transfer
         fi
 }
 
@@ -95,19 +105,11 @@ function do_sync(){
 function do_checks() {
         echo -e "\n$txt_ok Search for Local Pools..."
         LOCALPOOLSTMP="$($ZPOOL list -Ho name)"
-        ZFS_POOLS_SIZE_TMP=$LOCALPOOLSTMP
+
         for i in $LOCALPOOLSTMP; do
                 echo -e "$txt_info Found Pool: $bldblu $i"
                 PRODUCTION+=("$i")
         done
-
-        for i in $ZFS_POOLS_SIZE_TMP; do
-                ZFS_POOLS+=("$i")
-        done
-
-        if [[ "${ZFS_POOLS[*]}" =~ $TARGET* ]]; then            # if the target pool is (still)
-                ZFS_POOLS=(${ZFS_POOLS[@]/$TARGET*})            # mounted, remove it from array
-        fi
 
         if [[ "${PRODUCTION[*]}" =~ $TARGET ]]; then
                 echo -e "$txt_info Already mounted: $bldblu$TARGET"
