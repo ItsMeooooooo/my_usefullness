a83940fadb46d6d4682b8444e6822b98c83dcefc
ItsMe <itsme@bubbleclub.de>
2022-06-24 13:35:43 +0200

    after merge from github

---
+++ linux_backup/promtcolors.sh
@@ -0,0 +1,252 @@
+#!/bin/bash
+#
+# Name		: promtcolors.sh (the typo is intended
+#				  at least now)
+# Provides      : Creates a bash Startup Environment
+# Description   : Create a startup Environment, sets some
+#		  Aliases and defines some usefull functions
+# Location:	: /etc/profile.d/
+#
+# Modified      : 08|12|2020
+# Author        : ItsMe
+# Reply to      : itsme@bubbleclub.de           
+#
+# Comment:	: I did not test for POSIX Compliance
+#		  Most of the time I work in RHEL/ CentOS
+#		  Environments. Simply place the File
+#		  in your /etc/profile.d and it should be
+#		  parsed properly. You'll experience 
+#		  a different Behavior on Debian based
+#		  Distros. Here you have to edit
+#		  /etc/profile and add:
+#
+# 		  if [ -d /etc/profile.d ]; then
+#   			for i in /etc/profile.d/*.sh; do
+#     				if [ -r $i ]; then
+#       				.  $i
+#     				fi
+#   			done
+#   			unset i
+# 		  fi
+#
+#########################################################
+# 
+# OK - Lets Work
+#
+#########################################################
+
+
+# .bashrc
+# Reset
+Color_Off='\e[0m'       # Text Reset
+
+# Regular Colors
+Black='\e[0;30m'        # Black
+Red='\e[0;31m'          # Red
+Green='\e[0;32m'        # Green
+Yellow='\e[0;33m'       # Yellow
+Blue='\e[0;34m'         # Blue
+Purple='\e[0;35m'       # Purple
+Cyan='\e[0;36m'         # Cyan
+White='\e[0;37m'        # White
+
+# Bold
+BBlack='\e[1;30m'       # Black
+BRed='\e[1;31m'         # Red
+BGreen='\e[1;32m'       # Green
+BYellow='\e[1;33m'      # Yellow
+BBlue='\e[1;34m'        # Blue
+BPurple='\e[1;35m'      # Purple
+BCyan='\e[1;36m'        # Cyan
+BWhite='\e[1;37m'       # White
+
+# Underline
+UBlack='\e[4;30m'       # Black
+URed='\e[4;31m'         # Red
+UGreen='\e[4;32m'       # Green
+UYellow='\e[4;33m'      # Yellow
+UBlue='\e[4;34m'        # Blue
+UPurple='\e[4;35m'      # Purple
+UCyan='\e[4;36m'        # Cyan
+UWhite='\e[4;37m'       # White
+
+# Background
+On_Black='\e[40m'       # Black
+On_Red='\e[41m'         # Red
+On_Green='\e[42m'       # Green
+On_Yellow='\e[43m'      # Yellow
+On_Blue='\e[44m'        # Blue
+On_Purple='\e[45m'      # Purple
+On_Cyan='\e[46m'        # Cyan
+On_White='\e[47m'       # White
+
+# High Intensty
+IBlack='\e[0;90m'       # Black
+IRed='\e[0;91m'         # Red
+IGreen='\e[0;92m'       # Green
+IYellow='\e[0;93m'      # Yellow
+IBlue='\e[0;94m'        # Blue
+IPurple='\e[0;95m'      # Purple
+ICyan='\e[0;96m'        # Cyan
+IWhite='\e[0;97m'       # White
+
+# Bold High Intensty
+BIBlack='\e[1;90m'      # Black
+BIRed='\e[1;91m'        # Red
+BIGreen='\e[1;92m'      # Green
+BIYellow='\e[1;93m'     # Yellow
+BIBlue='\e[1;94m'       # Blue
+BIPurple='\e[1;95m'     # Purple
+BICyan='\e[1;96m'       # Cyan
+BIWhite='\e[1;97m'      # White
+
+# High Intensty backgrounds
+On_IBlack='\e[0;100m'   # Black
+On_IRed='\e[0;101m'     # Red
+On_IGreen='\e[0;102m'   # Green
+On_IYellow='\e[0;103m'  # Yellow
+On_IBlue='\e[0;104m'    # Blue
+On_IPurple='\e[10;95m'  # Purple
+On_ICyan='\e[0;106m'    # Cyan
+On_IWhite='\e[0;107m'   # White
+
+txtblk='\e[0;30m' # Black - Regular
+txtred='\e[0;31m' # Red
+txtgrn='\e[0;32m' # Green
+txtylw='\e[0;33m' # Yellow
+txtblu='\e[0;34m' # Blue
+txtpur='\e[0;35m' # Purple
+txtcyn='\e[0;36m' # Cyan
+txtwht='\e[0;37m' # White
+bldblk='\e[1;30m' # Black - Bold
+bldred='\e[1;31m' # Red
+bldgrn='\e[1;32m' # Green
+bldylw='\e[1;33m' # Yellow
+bldblu='\e[1;34m' # Blue
+bldpur='\e[1;35m' # Purple
+bldcyn='\e[1;36m' # Cyan
+bldwht='\e[1;37m' # White
+unkblk='\e[4;30m' # Black - Underline
+undred='\e[4;31m' # Red
+undgrn='\e[4;32m' # Green
+undylw='\e[4;33m' # Yellow
+undblu='\e[4;34m' # Blue
+undpur='\e[4;35m' # Purple
+undcyn='\e[4;36m' # Cyan
+undwht='\e[4;37m' # White
+bakblk='\e[40m'   # Black - Background
+bakred='\e[41m'   # Red
+badgrn='\e[42m'   # Green
+bakylw='\e[43m'   # Yellow
+bakblu='\e[44m'   # Blue
+bakpur='\e[45m'   # Purple
+bakcyn='\e[46m'   # Cyan
+bakwht='\e[47m'   # White
+txtrst='\e[0m'    # Text Reset
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias jeo='joe'
+alias sun="su -c 'su -s $(command -v bash) - nginx'"
+alias shred='shred -n 100 -z -v -u'
+alias docker='podman'
+
+# ignore Dupes and Lines with whitespaces
+# options are: ignorespace, ignoredups and ignoreboth
+HISTCONTROL=ignoreboth
+
+if [[ $EUID -eq 0 ]];
+	then
+	# if root set prompt to red
+		PS1="\[$bldred\][\u\[$bldwht\]@\[$bldred\]\h\[$bldylw\] \W\[$bldred\]]\[$bldylw\]\$\[$txtrst\] "; 
+	# if nginx another color and go to '/var/www/html
+	elif [[ $EUID -eq $(grep nginx /etc/passwd | grep -Eo '[0-9]{1,}' | head -n1) ]];
+		then 
+		PS1="\[$bldgrn\][\u\[$bldwht\]@\[$bldgrn\]\h\[$bldylw\] \W\[$bldgrn\]]\[$bldylw\]\$\[$txtrst\] ";
+	# colors for standard user(s)
+	else
+		PS1="\[$bldblu\][\u\[$bldwht\]@\[$bldblu\]\h\[$bldylw\] \W\[$bldblu\]]\[$bldylw\]\$\[$txtrst\] ";
+fi
+
+# export joe as default editor
+#
+# since my first Distro was a slackware and back in the Day
+# the only Editor I've found as a rookie was 'joe' I'm still
+# stuck with it
+if type joe > /dev/null; then
+    export EDITOR=/usr/bin/joe
+fi
+
+# some Functions
+function generate_ssl_cert {
+  cert_name=$1
+
+  (
+        openssl genrsa -des3 -out ${cert_name}.key 1024
+        openssl rsa -in ${cert_name}.key -out ${cert_name}.pem
+        openssl req -new -key ${cert_name}.pem -out ${cert_name}.csr
+        openssl x509 -req -days 365 -in ${cert_name}.csr -signkey ${cert_name}.pem -out ${cert_name}.crt
+  )
+}
+
+cd() {
+  if [ -n "$1" ]; then
+    builtin cd "$@" && ls --group-directories-first
+  else
+    builtin cd ~ && ls --group-directories-first
+  fi
+}
+
+function myip(){
+    curl ipv4.ownip.org
+    curl ipv6.ownip.org
+}
+
+function check_(){
+	cut -f1 -d" " ~/.bash_history | sort | uniq -c | sort -nr | head -n 30
+}
+
+function compress_() {
+   # Credit goes to: Daenyth
+   FILE=$1
+   shift
+   case $FILE in
+      *.tar.bz2) tar cjf $FILE $*  ;;
+      *.tar.gz)  tar czf $FILE $*  ;;
+      *.tgz)     tar czf $FILE $*  ;;
+      *.zip)     zip $FILE $*      ;;
+      *.rar)     rar $FILE $*      ;;
+      *)         echo "Filetype not recognized" ;;
+   esac
+}
+
+extract () {
+  if [ -f $1 ] ; then
+      case $1 in
+          *.tar.bz2)   tar xvjf $1    ;;
+          *.tar.gz)    tar xvzf $1    ;;
+          *.bz2)       bunzip2 $1     ;;
+          *.rar)       rar x $1       ;;
+          *.gz)        gunzip $1      ;;
+          *.tar)       tar xvf $1     ;;
+          *.tbz2)      tar xvjf $1    ;;
+          *.tgz)       tar xvzf $1    ;;
+          *.zip)       unzip $1       ;;
+          *.Z)         uncompress $1  ;;
+          *.7z)        7z x $1        ;;
+          *)           echo "don't know how to extract '$1'..." ;;
+      esac
+  else
+      echo "'$1' is not a valid file!"
+  fi
+}
+function serve (){
+	echo "Serving the Directory on Port 8000"
+	if ! type python3 > /dev/null; then 
+	    python -m SimpleHTTPServer 8000
+	else
+	    python -m http.server 8000
+	fi
+}
+
+# EOF
---
+++ linux_backup/rsync_inc.sh
@@ -0,0 +1,138 @@
+#!/usr/bin/env bash
+#
+# Provides      : Incremental Backup using rsync 
+# Description   : A script to perform incremental backups
+#                 using rsync.
+#                 
+#                                  
+# NAS Hostname  : 
+# NAS Mount:    : 
+#
+# Created       : 27|03|2021
+#
+# Modified      : 27|04|2021
+#                 - Added the Functionality to write to
+#                   two logfiles: debug and info
+#                 - Added Commandline Options
+#                 - some formatting Improvements
+#               : 03|07|2021
+#                 - Added a proper help funktion
+#                 - changed the location of logfiles
+#                 - changed the $SOURCE_DIR Standard location
+#                 - modified the rsync options (-HAXh --numeric-ids)
+#                 - addressed some minor issues
+#                 - Added Licence
+#
+# Client        : 
+# Author        : ItsMe
+# Reply to      : itsme@bubbleclub.de
+#
+# Licence       : MIT Licence
+#
+# Editor(s)     : vim + joe
+#########################################################
+#
+# OK - Lets Work
+#
+
+# set some bash env
+set -o errexit								                # equivalent to: "set -e" (make the script execution terminate)
+set -o nounset								                # equivalent to: "set -u" (terminate the script when a variable isn't set)
+set -o pipefail								                # make the whole pipe fail when a subcommand fails
+
+# set some variables
+SCRIPT_NAME="${0##*/}"                      				# this way we do not need to call the 'basename' function
+									                        # shellcheck disable=SC2034
+SCRIPT_SHORT_NAME="${SCRIPT_NAME%.*}"       				# the script name without extension
+
+
+SOURCE_DIR="$HOME/test"				        		        # set here the Source Directory you want to backup
+                                    					    # when the "-d" option on commandline isn't used
+
+BACKUP_DIR="/mnt/Backup"            					    # set here the Destination Directory to backup to
+                                    					    # when the "-b" option on commandline isn't used
+
+LOG_DIR=$BACKUP_DIR
+
+NOW="$(date '+%Y-%m-%d_%H:%M:%S')"
+readonly "$NOW"
+BACKUP_PATH="${BACKUP_DIR}/${NOW}"
+LATEST_LINK="${BACKUP_DIR}/latest"
+
+# check for the rsync binary and exit when not found
+RSYNC=$(command -v rsync)
+if [[ -z $? ]] ; then                                               	# "-z"  is the bash check for
+    echo -e "rsync isn't found\naborting..." | tee -a info.log      	#       an empty string
+    echo -e "rsync isn't found\naborting..." | tee -a debug.log     	# "$?"  is the bash variable
+    exit                                                            	#       for the exit code of the 
+fi                                                                  	#       last command
+
+# Options for rsync
+RSYNC_OPTS="-aHAXh --verbose --checksum --delete --numeric-ids"         # "-a"  equivalent to -rlptgoD
+                                                                        # "-r"  recursive
+                                                                        # "-l"  copy symlinks as symlinks
+                                                                        # "-p"  preserve permissions
+                                                                        # "-t"  preserve modification times
+                                                                        # "-g"  preserve group
+                                                                        # "-o"  preserve owner
+                                                                        # "-D"  same as --devices --specials
+                                                                        # "-h"  human readable
+                                                                        # "--numeric-ids" don't map uid/gid values by user/group name
+
+
+printf '' > "$LOG_DIR"/info.log						                    # Reset the log files on every run
+printf '' > "$LOG_DIR"/debug.log
+
+usage_help(){								                            # define a short help displayed when for "-h" option
+    echo "$SCRIPT_NAME - A script to perform incremental backups using rsync."
+    echo
+    echo "Usage  : $SCRIPT_NAME [ OPTIONS ]"
+    echo "Options:"
+    echo "options: -d   Source Directory"
+    echo "options: -b   Backup Directory"
+    echo "options: -h   Print this Help"
+    echo
+    echo "if no directories are set on the command line the defaults are used:"
+    echo "actual Source Directory: $SOURCE_DIR"
+    echo "actual Backup Directory: $BACKUP_DIR"
+}
+                                                                    
+while getopts "d:b:h" opt; do                                       	#       get Commandline Options
+  case $opt in                                                      	#       some are given
+    d)  if [ -n "$OPTARG" ]; then                                     	#       possible Options are:
+            SOURCE_DIR=$OPTARG                                      	# "-d"  Source Directory
+        fi                                                          	# "-b"  Backup Directory
+        ;;                                                          	# "-h"  for help
+    b)  if [ -n "$OPTARG" ]; then                                     	#       all Options are NOT mandatory
+            BACKUP_DIR=$OPTARG                                      	#       we set default values at the
+            BACKUP_PATH="${BACKUP_DIR}/${NOW}"                      	#       start of the script
+            LATEST_LINK="${BACKUP_DIR}/latest"                      
+        fi
+        ;;
+    h)  usage_help
+        exit 0
+        ;;
+        
+    \?) echo "Invalid option $opt" >&2
+        exit 1
+        ;;
+  esac
+done
+
+		
+exec 1>>"$LOG_DIR"/debug.log 2>>"$LOG_DIR"/debug.log			        # Redirect both stdout and stderr
+									                                    # write to the debug logfile
+
+									                                    # Write to both info and debug
+echo "Starting Backup Files at $NOW from $SOURCE_DIR" | tee -a "$LOG_DIR"/info.log
+
+									                                    # run the command
+$RSYNC "$RSYNC_OPTS" "${SOURCE_DIR}/" --link-dest="${LATEST_LINK}" --exclude=".cache" "${BACKUP_PATH}"
+
+rm -rf "${LATEST_LINK}"							                        # remove the symlink to the Directory 'latest'
+
+ln -s "${BACKUP_PATH}" "${LATEST_LINK}"					                # create the symlink from the last backup to 'latest'
+
+echo " ✅ " | tee -a "$LOG_DIR"/info.log				                   # Write to both info and debug
+
+# EOF
